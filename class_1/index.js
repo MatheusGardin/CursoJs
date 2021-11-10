8fcb21a0b983a1e85dd3c723ad18afb1e2705b60
MatheusGardin <jackmatheus123@gmail.com>
2021-11-10 12:51:34 -0300

    Class 1, console.log

---
+++ class_1/index.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+let text = 'Hello World 2';
+console.log(text);
+
+console.log('Name');
+console.log("Name");
+console.log(`Name`);
+
+console.log(123, 15.05, 'text', text);
+
+console.log(`Text: ${text}`);
