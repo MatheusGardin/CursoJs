485e598ddc0c89b515abcedd32b32a2b89f92241
MatheusGardin <jackmatheus123@gmail.com>
2021-11-11 12:39:27 -0300

    Using let

---
+++ class_3/index.js
@@ -0,0 +1,18 @@
+// console.log('João comprou um carro')
+// console.log('João se casou')
+// console.log('João teve um filho')
+// console.log('João gosta de carne')
+
+let nome = 'João';
+
+console.log(nome, 'comprou um carro');
+console.log('Em 2010', nome, 'se casou');
+console.log(nome, 'teve um filho');
+console.log(nome, 'gosta de carne');
+
+nome = 'Pedro';
+
+console.log();
+console.log(nome, 'vendeu um carro');
+console.log('Em 2010', nome, 'se separou');
+console.log(nome, 'gosta de salada');
